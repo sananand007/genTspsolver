1324a1e7be44c5048a9d3e658ba930909260d522
sananand007 <anandjairam@ufl.edu>
2019-01-16 07:54:31 -0800

    travis setup: u17

--- tests.py
+++ tests.py
@@ -9,8 +9,9 @@ from itertools import combinations_with_replacement, combinations, permutations
 from haversine import haversine
 import time
 from find_dist import Calculate_dist
-sys.path.append(os.path.join(os.path.dirname(__file__), "tsp-local_2opt"))
+#from tsp-local_2opt import Two_opt
 
 if __name__=="__main__":
 	csv_input = os.getcwd()
-	Obj = Two_opt(csv_input)
+	for f in os.list_dir(csv_input):
+		print(f)
